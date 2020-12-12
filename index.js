58fa700d86e1b67dadf4283bbe32a1e4adebd486
Joel John Kurien <55293875+joelkurien@users.noreply.github.com>
2020-12-12 12:34:36 +0530

    Member and patient doctor interaction

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require("express")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
@@ -16,34 +17,126 @@ const doctorSchema = new mongoose.Schema({
   name: String,
   phone: Number,
   medtype: String,
-  licenseNo:String,
-  hospital:String,
-  TimingFrom:Number,
-  TimingTo:Number,
-  Experiance:Number
-});
-
-const Doctor = mongoose.model("doctor", doctorSchema);
-
-app.get('/', function(req, res){
-  res.render("doctor")
 })
 
-app.post('/', function(req, res){
-  let doc = Doctor({
-    name: req.body.doctorName,
-    phone: req.body.doctorPhone,
-    medtype: req.body.medtype,
-    licenseNo:req.body.licenseNo,
-    hospital:req.body.hospital,
-    TimingFrom:req.body.TimingFrom,
-    TimingTo:req.body.TimingTo,
-    Experiance:req.body.Experiance
+const Doctor = mongoose.model("doctor", doctorSchema)
+
+app.route('/doctor')
+  .get(function(req, res){
+    res.render("doctor")
   })
 
-  doc.save(res.render("success"));
+  .post(function(req, res){
+    let doc = Doctor({
+      name: req.body.doctorName,
+      phone: req.body.doctorPhone,
+      medtype: req.body.medtype,
+    })
+
+    doc.save(res.render("success"));
+  })
+
+app.route('/logindoc')
+  .get(function(req, res){
+    res.render("login-doc")
+  })
+
+  .post(function(req, res){
+    let loginname = req.body.docName;
+    let loginphone = req.body.docNumber;
+    Doctor.findOne({name: loginname},function(err, doctor){
+      if(!err){
+        if(doctor == null){
+          res.redirect('/logindoc')
+        } else {
+          let obj = doctor
+          if(obj.phone == loginphone){
+            res.render("success")
+          } else {
+            res.redirect('/logindoc')
+          }
+        }
+      } else {
+        console.log(err)
+      }
+    })
+  })
+
+const memberSchema = new mongoose.Schema({
+  name: String,
+  password: String,
+  phone: Number,
+  age: Number
 })
 
+const Member = mongoose.model("member", memberSchema)
+
+app.route('/member')
+  .get(function(req, res){
+      res.render("member")
+  })
+
+  .post(function(req, res){
+      let memb = Member({
+        name: req.body.patientName,
+        password: req.body.patientPassword,
+        phone: req.body.patientPhone,
+        age: req.body.age
+      })
+
+      memb.save(res.render("success"))
+  })
+
+app.route('/loginmem')
+   .get(function(req, res){
+     res.render("login-mem")
+   })
+
+   .post(function(req, res){
+     let loginname = req.body.membName;
+     let loginpassword = req.body.membPassword;
+     Member.findOne({name: loginname},function(err, member){
+       if(!err){
+         if(member == null){
+           res.redirect('/loginmem')
+         } else {
+           let obj = member
+           if(obj.password == loginpassword){
+             res.render("success")
+           } else {
+             res.redirect('/loginmem')
+           }
+         }
+       } else {
+         console.log(err)
+       }
+     })
+   })
+
+const appointmentSchema = mongoose.Schema({
+  age: Number,
+  doc: String,
+  symptoms: String,
+  timings: String
+})
+
+const Appointment = mongoose.model('appointment', appointmentSchema)
+
+app.route('/appointment')
+  .get(function(req, res){
+    res.render("patdoc")
+  })
+
+  .post(function(req, res){
+    const appoint = Appointment({
+      age:req.body.age,
+      doc:req.body.doc,
+      symptoms:req.body.symptoms,
+      timings:req.body.timeSlot
+    })
+
+    appoint.save(res.render("Success"))
+  })
 
 app.listen(3000, function(err){
   console.log("The server is listening")
